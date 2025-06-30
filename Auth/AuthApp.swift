cf29a3a36727dfd49b0c0b4d7f4cc413657eb82b
willpotter1 <willfspot@yahoo.co.uk>
2025-06-30 13:36:42 -0400

    Initial Commit

---
+++ Auth/AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthApp.swift
+//  Auth
+//
+//  Created by William Potter on 2025-06-30.
+//
+
+import SwiftUI
+
+@main
+struct AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Auth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Auth
+//
+//  Created by William Potter on 2025-06-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
