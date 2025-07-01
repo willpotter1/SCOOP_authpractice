3988656ef07b67cd3491cb8f49c1b22ff18e95ce
willpotter1 <willfspot@yahoo.co.uk>
2025-07-01 14:33:45 -0400

    Setup and connecting to firebase. No authentication added yet but added auth page

--- Auth/AuthApp.swift
+++ Auth/AuthApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import Firebase
+import UIKit
+
 
 @main
 struct AuthApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+      print("Configed")
+    return true
+  }
+}
+
---
+++ Auth/Authentication/AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  Auth
+//
+//  Created by William Potter on 2025-07-01.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
