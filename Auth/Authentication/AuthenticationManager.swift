902e8219352caae3cb49ad261fec77037db20427
willpotter1 <willfspot@yahoo.co.uk>
2025-07-01 17:11:02 -0400

    Added sign in pages and created authentication manager

---
+++ Auth/Authentication/AuthenticationManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationManager.swift
+//  Auth
+//
+//  Created by William Potter on 2025-07-01.
+//
+
+import Foundation
---
+++ Auth/Authentication/SignInEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInEmailView.swift
+//  Auth
+//
+//  Created by William Potter on 2025-07-01.
+//
+
+import SwiftUI
+
+struct SignInEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInEmailView()
+}
